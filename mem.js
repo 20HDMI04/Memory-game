9caf5dd798c3dc1f2aacfbb9fc3761a6f02d8ebf
20HDMI40 <heropista@gmail.com>
2022-05-28 11:57:16 +0200

    Timer,Start Button

--- mem.js
+++ mem.js
@@ -60,10 +60,20 @@ function onCardClicked(e) {
         combosFound++;
         previousCard = null;
         if (combosFound === 8) {
-            function timeFunction() {
-                setTimeout(function(){ alert("You Win!"); }, 1000);
-            }
+                clearInterval(timerVariable);
+                setTimeout(function(){ alert(`You Win! Your Time IS: ${ timerVariable} `); }, 1000);
+              }
             timeFunction()
         }
     }
+
+var timerVariable = setInterval(countUpTimer, 1000);
+var totalSeconds = 0;
+
+function countUpTimer() {
+  ++totalSeconds;
+  var hour = Math.floor(totalSeconds / 3600);
+  var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+  var seconds = totalSeconds - (hour * 3600 + minute * 60);
+  document.getElementById("count_up_timer").innerHTML = hour + ":" + minute + ":" + seconds;
 }
